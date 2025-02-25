86d283fa48c349ee344a6a42b94a0a647639ed98
DangerKrip <kostyashka2003@gmail.com>
2025-02-25 16:27:20 +0200

    Some dummy changes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
-import MailBox from "./MailBox";
+import Product from "./Product";
 
 function App() {
   return (
     <>
       <div>
-        <MailBox></MailBox>
+        <Product name={"Product 1"} price={1000}></Product>
+        <Product name={"Product 2"} price={340}></Product>
+        <Product name={"Product 3"} price={134}></Product>
       </div>
     </>
   );
